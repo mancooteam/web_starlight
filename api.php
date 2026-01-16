b65c1a229d6ff1cc7c5acecabfdb2aff99ab1621
nath <karolinamariabodziony@gmail.com>
2026-01-16 13:15:26 +0100

    js json moment

--- api.php
+++ api.php
@@ -1,9 +1,5 @@
 <?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
--- script/script.js
+++ script/script.js
@@ -1,6 +1,12 @@
 async function obtenerDatos() {
     try {
-        let res = await fetch("../api.php");
+        let res = await fetch("../api.php", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(datos)
+        });
 
         if(!res.ok) {
             throw new Error("Error con el servidor");
