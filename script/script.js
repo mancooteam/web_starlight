b97a48f9b62a6edb97fde35b509731f394532b97
nath <karolinamariabodziony@gmail.com>
2026-01-16 13:33:36 +0100

    api connection test

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 async function fetchData() {
     try {
-        const response = await fetch('web-starlight-puf4xcplo-mancoteams-projects.vercel.app/api/api.php');
+        const response = await fetch('/api/api.php');
         if (!response.ok) {
             throw new Error(`HTTP error!`);
         }
