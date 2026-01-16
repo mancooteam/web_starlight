e9c24037093b3dec3e2e89c427630831644367e2
nath <karolinamariabodziony@gmail.com>
2026-01-16 13:09:35 +0100

    new attempt db

--- script/script.js
+++ script/script.js
@@ -1,11 +1,15 @@
-let obtenerDatos = () =>{
+async function obtenerDatos() {
     try {
         let res = await fetch("../api.php");
 
         if(!res.ok) {
             throw new Error("Error con el servidor");
         }
+    } catch (e) {
+        console.error("Error: " + e);
     }
+    let info = await res.json();
+    console.log("info");
 }
 
 document.addEventListener("DOMContentLoaded", () => {
