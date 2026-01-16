bf16b23898ed5b886f5ccfe3a96c735c85068ed6
nath <karolinamariabodziony@gmail.com>
2026-01-16 13:39:55 +0100

    apparent routing problem :)

--- api/api.php
+++ api/api.php
@@ -1,9 +1,5 @@
 <?php
-
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST');
-header('Access-Control-Allow-Headers: Content-Type');
 
 $datos = [];
 $db = getenv('DB_PASSWORD');
