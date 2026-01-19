591170e8e91376122a77c0ca47913f7c84754187
mancooteam <nathsnewstyle@gmail.com>
2026-01-19 21:45:29 +0100

    What am i even doing

--- js/app.js
+++ js/app.js
@@ -1,34 +1,42 @@
 const createCard = (e) => {
     const postac = document.createElement('postac');
+
     const avek = document.createElement('img');
     avek.src = e.avek;
+
     const imie = document.createElement('staty');
-    imie.innerText = e.imie
+    imie.innerText = e.imie;
+
     const staty = document.createElement('staty');
+
     const klan = document.createElement('klan');
     klan.innerText = e.klan;
     klan.classList.add(e.klan);
-    const toyhouse = document.createElement('toyhouse');
-    toyhouse.innerText = e.toyhouse;
 
-    staty.appendChild(klan, toyhouse);
-    postac.appendChild(avek, imie,staty);
+    const toyhouse = document.createElement('toyhouse');
+    toyhouse.innerText = e.toyhouse || '';
+    staty.append(klan, toyhouse);
+    postac.append(avek, imie, staty);
 
     document.getElementById('spis').appendChild(postac);
 }
 
 async function fetchData() {
     try {
-        const response = await fetch('../api/api.php')
+        const response = await fetch('../api/api.php');
         if (!response.ok) {
             throw new Error(`HTTP error!`);
         }
         const data = await response.json();
         console.log("Data received:", data);
-        data.forEach((e) => {
-            createCard(e);
-        })
-        
+        if (data.message) {
+            data.message.forEach((e) => {
+                createCard(e);
+            });
+        } else {
+            console.error("No 'message' array found in response");
+        }
+
     } catch (error) {
         console.log("Could not fetch data:" + error);
     }
