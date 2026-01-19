95b9920dd0e2b09ebae7fbae798e85f2603fb020
mancooteam <nathsnewstyle@gmail.com>
2026-01-19 21:20:09 +0100

    Data from db and insert into base

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
+const createCard = (e) => {
+    const postac = document.createElement('postac');
+    const avek = document.createElement('img');
+    avek.src = e.avek;
+    const imie = document.createElement('staty');
+    imie.innerText = e.imie
+    const staty = document.createElement('staty');
+    const klan = document.createElement('klan');
+    klan.innerText = e.klan;
+    klan.classList.add(e.klan);
+    const toyhouse = document.createElement('toyhouse');
+    toyhouse.innerText = e.toyhouse;
+
+    staty.appendChild(klan, toyhouse);
+    postac.appendChild(avek, imie,staty);
+
+    document.getElementById('spis').appendChild(postac);
+}
+
 async function fetchData() {
     try {
         const response = await fetch('../api/api.php')
@@ -6,7 +25,10 @@ async function fetchData() {
         }
         const data = await response.json();
         console.log("Data received:", data);
-        document.body.innerHTML = `<h1>${data.message}</h1>`;
+        const postacie = JSON.parse(data);
+        postacie.forEach((element) => {
+            createCard(element);
+        })
         
     } catch (error) {
         console.log("Could not fetch data:" + error);
