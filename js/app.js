5febd75d5777cabf2759f723a7da454565c53bf0
mancooteam <nathsnewstyle@gmail.com>
2026-01-19 22:14:03 +0100

    Typo fix

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ const createCard = (e) => {
 
     const klan = document.createElement('klan');
     klan.innerText = e.klan;
-    klan.classList.add(e.klan,klany);
+    klan.classList.add(e.klan,"klany");
 
     const toyhouse = document.createElement('toyhouse');
     toyhouse.innerText = e.toyhouse || '';
