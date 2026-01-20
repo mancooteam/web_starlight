1a86a0429e0adfe7789a1e7bcf5b88ff19690c15
mancooteam <nathsnewstyle@gmail.com>
2026-01-20 18:42:39 +0100

    i was an idiot

--- js/mod.js
+++ js/mod.js
@@ -1,11 +1,15 @@
 let showProfile = (p) => {
-
+    if(document.getElementById("imie")) document.getElementById("imie").value = p.imie || '';
+    if(document.getElementById("klan")) document.getElementById("klan").value = p.klan || 'cien';
+    if(document.getElementById("ranga")) document.getElementById("ranga").value = p.ranga || '';
+    if(document.getElementById("avek")) document.getElementById("avek").value = p.avek || '';
+    if(document.getElementById("toyhouse")) document.getElementById("toyhouse").value = p.toyhouse || '';
+    if(document.getElementById("id")) document.getElementById("id").value = p.id;
 }
 async function fetchData() {
     try {
         const queryString = window.location.search;
         const urlParams = new URLSearchParams(queryString);
-
         const id = urlParams.get('id');
 
         document.getElementById("id").value = id;
@@ -19,7 +23,6 @@ async function fetchData() {
                 throw new Error(`HTTP error!`);
             }
             const text = await response.json();
-            const data = JSON.parse(text);
             if (data.message) {
                 data.message.forEach((e) => {
                     showProfile(e);
