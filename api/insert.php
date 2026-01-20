306cfbe7e266bc75c4230dbff1376dab507407f1
mancooteam <nathsnewstyle@gmail.com>
2026-01-20 07:26:07 +0100

    INSERT MOD

--- api/insert.php
+++ api/insert.php
@@ -1,33 +1,44 @@
 <?php
 // api/insert.php
 
+ini_set('display_errors', 0);
+error_reporting(E_ALL);
+
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 
 try {
-    require_once 'bd.php';
+    if (file_exists(__DIR__ . '/../bd.php')) {
+        require_once __DIR__ . '/../bd.php';
+    } elseif (file_exists('../bd.php')) {
+        require_once '../bd.php';
+    } else {
+        throw new Exception("Error crítico: No se encuentra el archivo bd.php en la ruta ../bd.php");
+    }
 
     if (!isset($pdo)) {
-        throw new Exception("No se pudo conectar: la variable \$pdo no está definida en bd.php");
+        throw new Exception("El archivo bd.php se cargó, pero la variable \$pdo no existe.");
     }
 
     $inputJSON = file_get_contents('php://input');
     $input = json_decode($inputJSON, true);
 
-    // Validar datos mínimos requeridos
-    if (empty($input['imie']) || empty($input['klan'])) {
-        throw new Exception("Faltan datos obligatorios (Imie o Klan).");
+    if (json_last_error() !== JSON_ERROR_NONE) {
+        throw new Exception("JSON inválido.");
     }
+
+    // Preparar SQL
     $sql = "INSERT INTO st_postac (id, imie, klan, ranga, avek, toyhouse)
             VALUES (:id, :imie, :klan, :ranga, :avek, :toyhouse)";
 
     $stmt = $pdo->prepare($sql);
 
+    // Ejecutar
     $stmt->execute([
-        ':id'       => $input['id'],
-        ':imie'     => $input['imie'],
-        ':klan'     => $input['klan'],
+        ':id'       => $input['name'] ?? null,
+        ':imie'     => $input['imie'] ?? null,
+        ':klan'     => $input['klan'] ?? null,
         ':ranga'    => $input['ranga'] ?? '',
         ':avek'     => $input['avek'] ?? '',
         ':toyhouse' => $input['toyhouse'] ?? ''
