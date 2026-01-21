5fbe4a380af83fc74a73e338a7dbc2e3fbd86474
nath <karolinamariabodziony@gmail.com>
2026-01-21 09:20:16 +0100

    Pathing problem

--- api/consult.php
+++ api/consult.php
@@ -3,7 +3,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-require_once '/db.php';
+require_once 'db.php';
 
 try {
     $pdo = getDBConnection();
